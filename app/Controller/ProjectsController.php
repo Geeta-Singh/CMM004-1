4a100cf613396eae3fd2a6723eb98a3b25da25b8
Geeta-Singh <geetasinghrajput99@gmail.com>
2015-04-25 02:59:10 +0530

    Update ProjectsController.php

--- app/Controller/ProjectsController.php
+++ app/Controller/ProjectsController.php
@@ -5,12 +5,9 @@
  * Date: 09/03/15
  * Time: 23:08
  */
-
 App::uses('AppController', 'Controller');
 class ProjectsController extends AppController {
-
     public $uses = array('Project','Task','User'); // Select which model to use
-
     public function index($project_id) {
         $this->set('tasks', $this->Task->findAllByProjectId($project_id));
         $this->set('projectusers', $this->Project->findAllById($project_id));
@@ -22,15 +19,24 @@ class ProjectsController extends AppController {
         ]));
         $this->set('project_role', $this->User->ProjectsUser->findByUserIdAndProjectId($this->Auth->user('id'),$project_id));
     }
-
     public function create () {
         if ( $this->request->is('post') ) {
             print_r($this->request->data);
-            $this->Project->save($this->request->data);
+          if ($this->Project->save($this->request->data)) {
+                $this->Session->setFlash("User has been created");
+            } else { // If unable to save user, display message
+                $this->Session->setFlash("Unable to create user account");
+            }
+           // $this->Project->save($this->request->data);
         }
     }
 
+    
     public function delete($id) {
         $this->Post->delete($id);
+    }
+    public function edit() {
+	 $projectData = $this->request->data['Project'];
+        $this->Task->save($projectData);
     }
 }
